6a85cc2813b2f6e6bcdcf1929160517528572baf
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-07-07 23:07:58 -0500

    feat: create the User model

--- models/User.php
+++ models/User.php
@@ -1,1 +1,42 @@
 <?php
+    class User{
+        private $conn;
+        private $table = 'Users';
+
+        public $id;
+        public $password;
+        public $username;
+        public $fullname;
+
+        public function __construct($db){
+            $this->conn = $db;
+        }
+
+        public function read_single(){
+            $query = 'SELECT
+                *
+            FROM
+                '.$this->table.'
+            WHERE
+                username = :username';
+
+            $stmt = $this->conn->prepare($query);
+
+            $stmt->bindParam(':username', $this->username);
+
+            $stmt->execute();
+
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($user && password_verify($this->password, $user['password'])){
+                $this->fullname = $user['fullname'];
+            }
+            else{
+                die(json_encode([
+                    'res' => false,
+                    'msg' => 'Bad password/username'
+                ]));
+            }
+            
+        }
+    }
